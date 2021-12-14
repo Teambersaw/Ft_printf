f3d0b1309568e3afe0b6059a00b64cc64898274a
Jules ROSSETTI <jrossett@e2r3p4.clusters.42paris.fr>
2021-12-14 17:21:24 +0100

    oui

--- ft_printf.c
+++ ft_printf.c
@@ -3,74 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: teambersaw <teambersaw@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/11 16:55:30 by teambersaw        #+#    #+#             */
-/*   Updated: 2021/12/13 19:38:21 by teambersaw       ###   ########.fr       */
+/*   Created: 2021/12/14 15:02:35 by jrossett          #+#    #+#             */
+/*   Updated: 2021/12/14 16:11:30 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "ft_printf.h"
 
-
-
-
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
+int	ft_args(const char	*str, va_list args, int *i)
+{	
+	while (*str)
+	{
+		if (*str == '%' && *(str + 1) == 'c')
+			ft_putchar(va_arg(args, int), i);
+		else if (*str == '%' && *(str + 1) == 's')
+			ft_putstr(va_arg(args, char *), i);
+		else if (*str == '%' && *(str + 1) == 'p')
+			va_arg(args, void *);
+		else if (*str == '%' && (*(str + 1) == 'd' || *(str + 1) == 'i'))
+			ft_putnbr(va_arg(args, int), i);
+		else if (*str == '%' && *(str + 1) == 'u')
+			ft_putnbr_us(va_arg(args, unsigned int), i);
+		else if (*str == '%' && *(str + 1) == 'x')
+			ft_putnbr_base(va_arg(args, int), HEXA, i);
+		else if (*str == '%' && *(str + 1) == 'X')
+			ft_putnbr_base(va_arg(args, int), CHEXA, i);
+		else if (*str == '%' && *(str + 1) != '%')
+			str++;
+		else if (*str == '%' && *(str + 1) == '%')
+			ft_putchar(*str++, i);
+		else
+			ft_putchar(*str, i);
+		str++;
+	}
 }
 
-int ft_length(const char  *str)
+int	ft_printf(const char *str, ...)
 {
-    int length;
-    int i;
+	va_list	args;
+	int		i;
 
-    i = 0;
-    length = 0;
-
-    if (str == NULL)
-        return (0);
-    while (str[i])
-    {
-        if (str[i] == '%' && (str[i + 1] == 's' || str[i + 1] == 'c' || str[i + 1] == 'p' || str[i + 1] == 'd' || str[i + 1] == 'i' || str[i + 1] == 'u' || str[i + 1] == 'x'))
-        {
-            length++;
-            i += 2;
-        }
-        else if (str[i] == '%' && str[i + 1] == '%')
-        {
-            ft_putchar_fd(str[i], 1);
-            i += 2;
-        }
-        else
-            ft_putchar_fd(str[i++], 1);
-    }    
-    return (length);
+	i = 0;
+	va_start(args, str);
+	ft_args(str, args, &i);
+	va_end(args);
+	return (i);
 }
 
-int ft_printf(const char *str, ...)
+int	main(void)
 {
-    va_list args;
-    int     i;
-    int     j;
-
-    i = ft_length(str);
-    j = 0;
-    va_start(args, i);
-
-    while (j < i)
-    {
-        va_arg(args, int);
-        j++;
-    }
-    va_end(args);
-}
-
-
-#include <stdio.h>
-int main()
-{
-    ft_printf("u");
-    return (0);
+	char str[] = "coucou";
+	printf("%p\n", str);
+	printf("%lx", (unsigned long)str);
+	ft_printf("u\n");
+	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -3,20 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: teambersaw <teambersaw@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 17:00:52 by teambersaw        #+#    #+#             */
-/*   Updated: 2021/12/13 18:25:32 by teambersaw       ###   ########.fr       */
+/*   Updated: 2021/12/14 16:16:50 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# define HEXA 0123456789abcdef
+# define CHEXA 0123456789ABCDEF
+
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
 
-int ft_printf(const char *str, ...);
+int		ft_printf(const char *str, ...);
+void	ft_putchar(char c, int *i);
+void	ft_putstr(char *s, int *i);
+void	ft_putnbr_us(unsigned int n, int *i);
+void	ft_putnbr(int n, int *i);
+int		ft_args(const char	*str, va_list args, int *i);
 
 #endif
