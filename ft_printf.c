258014859a9a0a6bd8ac2fe600e8dc451bfd9e35
Jules ROSSETTI <jrossett@e2r4p2.clusters.42paris.fr>
2021-12-15 15:29:30 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,35 +6,76 @@
 /*   By: jrossett <jrossett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:02:35 by jrossett          #+#    #+#             */
-/*   Updated: 2021/12/14 16:11:30 by jrossett         ###   ########.fr       */
+/*   Updated: 2021/12/15 15:28:01 by jrossett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "ft_printf.h"
 
-int	ft_args(const char	*str, va_list args, int *i)
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s)
+	{
+		while (s[i])
+			i++;
+	}
+	return (i);
+}
+
+void	ft_putnbr_base(unsigned long n, const char *base, int *i)
+{
+	size_t	j;
+
+	j = ft_strlen(base);
+	if (n > 0 && n < j)
+		ft_putchar(base[n], i);
+	else
+	{
+		ft_putnbr_base(n / j, base, i);
+		ft_putnbr_base(n % j, base, i);
+	}
+}
+
+void	ft_pointeur(void *str, int *i)
+{
+	unsigned long int	p;
+
+	p = (unsigned long int)str;
+	if (!p)
+	{
+		ft_putstr(NULL_PTR, i);
+		return ;
+	}
+	ft_putstr("0x", i);
+	ft_putnbr_base(p, HEXA, i);
+}
+
+void	ft_args(const char	*str, va_list args, int *i)
 {	
 	while (*str)
 	{
 		if (*str == '%' && *(str + 1) == 'c')
 			ft_putchar(va_arg(args, int), i);
-		else if (*str == '%' && *(str + 1) == 's')
+		if (*str == '%' && *(str + 1) == 's')
 			ft_putstr(va_arg(args, char *), i);
-		else if (*str == '%' && *(str + 1) == 'p')
-			va_arg(args, void *);
-		else if (*str == '%' && (*(str + 1) == 'd' || *(str + 1) == 'i'))
+		if (*str == '%' && *(str + 1) == 'p')
+			ft_pointeur(va_arg(args, void *), i);
+		if (*str == '%' && (*(str + 1) == 'd' || *(str + 1) == 'i'))
 			ft_putnbr(va_arg(args, int), i);
-		else if (*str == '%' && *(str + 1) == 'u')
+		if (*str == '%' && *(str + 1) == 'u')
 			ft_putnbr_us(va_arg(args, unsigned int), i);
-		else if (*str == '%' && *(str + 1) == 'x')
+		if (*str == '%' && *(str + 1) == 'x')
 			ft_putnbr_base(va_arg(args, int), HEXA, i);
-		else if (*str == '%' && *(str + 1) == 'X')
+		if (*str == '%' && *(str + 1) == 'X')
 			ft_putnbr_base(va_arg(args, int), CHEXA, i);
-		else if (*str == '%' && *(str + 1) != '%')
+		if (*str == '%' && *(str + 1) == '%')
+			ft_putchar(*str, i);
+		if (*str == '%')
 			str++;
-		else if (*str == '%' && *(str + 1) == '%')
-			ft_putchar(*str++, i);
 		else
 			ft_putchar(*str, i);
 		str++;
@@ -52,12 +93,15 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (i);
 }
-
+/*
 int	main(void)
 {
-	char str[] = "coucou";
+	int i;
+	char *str = 0;
+
+	i = 0;
 	printf("%p\n", str);
-	printf("%lx", (unsigned long)str);
-	ft_printf("u\n");
+	ft_pointeur(str, &i);
 	return (0);
 }
+*/
